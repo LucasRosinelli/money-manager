f8c7c90ad107172ede7adad988ba1064aec26523
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-26 14:31:08 +0100

    DbContext service configured.

--- src/MoneyManager.Api/Startup.cs
+++ src/MoneyManager.Api/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MoneyManager.Api.Infrastructure.Persistence.DataContexts;
 
 namespace MoneyManager.Api
 {
@@ -17,6 +19,10 @@ namespace MoneyManager.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MoneyManagerDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("money-manager");
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
