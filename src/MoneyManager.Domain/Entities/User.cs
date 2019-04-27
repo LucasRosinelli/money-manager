0ee20ec23dfa33264ee53c79a455951e06a93c7c
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-27 10:24:33 +0100

    User entity created.

---
+++ src/MoneyManager.Domain/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoneyManager.Domain.Entities
+{
+    public class User
+    {
+        public long Id { get; private set; }
+        public Guid Identifier { get; private set; }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public string FullName { get; private set; }
+        public DateTimeOffset CreatedOn { get; private set; }
+        public DateTimeOffset? LastUpdatedOn { get; private set; }
+        public bool IsActive { get; private set; }
+
+        protected User()
+        {
+            this.Identifier = Guid.NewGuid();
+            this.CreatedOn = DateTimeOffset.Now;
+            this.IsActive = true;
+        }
+
+        public User(string login, string password, string fullName)
+            : this()
+        {
+            this.Login = login;
+            this.Password = password;
+            this.FullName = fullName;
+        }
+    }
+}
