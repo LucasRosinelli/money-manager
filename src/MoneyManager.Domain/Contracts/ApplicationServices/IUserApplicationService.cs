2ce543e841d200f8e73b2b7073a5d6b2f8c1714f
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-29 10:23:11 +0100

    User application service created.

---
+++ src/MoneyManager.Domain/Contracts/ApplicationServices/IUserApplicationService.cs
@@ -0,0 +1,9 @@
+﻿using MoneyManager.Domain.Contracts.Repositories;
+using MoneyManager.Domain.Entities;
+
+namespace MoneyManager.Domain.Contracts.ApplicationServices
+{
+    public interface IUserApplicationService : IApplicationServiceExtended<IUserRepository, User>
+    {
+    }
+}
