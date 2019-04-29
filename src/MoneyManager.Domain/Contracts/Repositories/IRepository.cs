d4e0b4a16210585e78ba92c9cdf848cf832d4469
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-29 08:52:41 +0100

    Repository contract created.

---
+++ src/MoneyManager.Domain/Contracts/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using MoneyManager.Domain.Contracts.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace MoneyManager.Domain.Contracts.Repositories
+{
+    public interface IRepository<TEntity>
+        where TEntity : IEntity
+    {
+        IEntity Create(TEntity entity);
+        IEntity Update(TEntity entity);
+        IEntity GetById(long id);
+        IEntity GetById(Guid identifier);
+        IEnumerable<IEntity> Get();
+    }
+}
