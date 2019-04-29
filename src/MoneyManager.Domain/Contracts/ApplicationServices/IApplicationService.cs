f27c31e9730a14cef655349834e40e0740b7442e
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-04-29 10:17:11 +0100

    Application service contracts created.

---
+++ src/MoneyManager.Domain/Contracts/ApplicationServices/IApplicationService.cs
@@ -0,0 +1,11 @@
+﻿using MoneyManager.Domain.Contracts.Entities;
+using MoneyManager.Domain.Contracts.Repositories;
+
+namespace MoneyManager.Domain.Contracts.ApplicationServices
+{
+    public interface IApplicationService<TRepository, TEntity>
+        where TRepository : IRepository<TEntity>
+        where TEntity : IEntity
+    {
+    }
+}
---
+++ src/MoneyManager.Domain/Contracts/ApplicationServices/IApplicationServiceExtended.cs
@@ -0,0 +1,11 @@
+﻿using MoneyManager.Domain.Contracts.Entities;
+using MoneyManager.Domain.Contracts.Repositories;
+
+namespace MoneyManager.Domain.Contracts.ApplicationServices
+{
+    public interface IApplicationServiceExtended<TRepository, TEntity>
+        where TRepository : IRepositoryExtended<TEntity>
+        where TEntity : IEntity
+    {
+    }
+}
