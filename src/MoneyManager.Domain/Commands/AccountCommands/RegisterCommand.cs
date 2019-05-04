2cac8ef3d978716d6b1342b9130d18ad1a2e7c02
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-05-04 20:20:06 +0100

    Account application service created.

---
+++ src/MoneyManager.Domain/Commands/AccountCommands/RegisterCommand.cs
@@ -0,0 +1,27 @@
+﻿using MoneyManager.Domain.Contracts.Commands;
+using System;
+
+namespace MoneyManager.Domain.Commands.AccountCommands
+{
+    public class RegisterCommand : ICommand
+    {
+        public Guid User { get; set; }
+        public string ShortName { get; set; }
+        public string LongName { get; set; }
+        public string Color { get; set; }
+        public string Icon { get; set; }
+
+        public RegisterCommand()
+        {
+        }
+
+        public RegisterCommand(Guid user, string shortName, string longName, string color, string icon)
+        {
+            this.User = user;
+            this.ShortName = shortName;
+            this.LongName = longName;
+            this.Color = color;
+            this.Icon = icon;
+        }
+    }
+}
---
+++ src/MoneyManager.Domain/DataTransferObjects/AccountDataTransferObjects/AccountDetailDataTransferObject.cs
@@ -0,0 +1,72 @@
+﻿using MoneyManager.Domain.Contracts.DataTransferObjects;
+using MoneyManager.Domain.Entities;
+using MoneyManager.Domain.Enums;
+using System;
+
+namespace MoneyManager.Domain.DataTransferObjects.AccountDataTransferObjects
+{
+    /// <summary>
+    /// Account detail.
+    /// </summary>
+    public class AccountDetailDataTransferObject : IDataTransferObject
+    {
+        /// <summary>
+        /// Account unique identifier.
+        /// </summary>
+        public Guid Identifier { get; set; }
+        /// <summary>
+        /// Account short name.
+        /// </summary>
+        public string ShortName { get; set; }
+        /// <summary>
+        /// Account long name.
+        /// </summary>
+        public string LongName { get; set; }
+        /// <summary>
+        /// Account RGB color.
+        /// </summary>
+        public string Color { get; set; }
+        /// <summary>
+        /// Account icon.
+        /// </summary>
+        public string Icon { get; set; }
+        /// <summary>
+        /// Date, time and timezone when the account was created.
+        /// </summary>
+        public DateTimeOffset CreatedOn { get; set; }
+        /// <summary>
+        /// Date, time and timezone when the account data was last updated.
+        /// </summary>
+        public DateTimeOffset? LastUpdatedOn { get; set; }
+        /// <summary>
+        /// Account status.
+        /// </summary>
+        public DefaultState Status { get; set; }
+
+        /// <summary>
+        /// Initializes an empty account.
+        /// </summary>
+        public AccountDetailDataTransferObject()
+        {
+        }
+
+        /// <summary>
+        /// Initializes an account based on user entity.
+        /// </summary>
+        /// <param name="account">Account data to be used.</param>
+        public AccountDetailDataTransferObject(Account account)
+        {
+            if (account != null)
+            {
+                this.Identifier = account.Identifier;
+                this.ShortName = account.ShortName;
+                this.LongName = account.LongName;
+                this.Color = account.Color;
+                this.Icon = account.Icon;
+                this.CreatedOn = account.CreatedOn;
+                this.LastUpdatedOn = account.LastUpdatedOn;
+                this.Status = account.Status;
+            }
+        }
+    }
+}
