f981536dff6a631f0f52869c8867a2c4f9d65341
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-05-06 10:48:32 +0100

    Entities Entry, Income and Expense added.

---
+++ src/MoneyManager.Domain/Entities/Entry.cs
@@ -0,0 +1,82 @@
+﻿using MoneyManager.Domain.Contracts.Entities;
+using MoneyManager.Domain.Enums;
+using MoneyManager.Domain.Scopes;
+using System;
+
+namespace MoneyManager.Domain.Entities
+{
+    public abstract class Entry : IEntityExtended<DefaultState>
+    {
+        public long Id { get; private set; }
+        public Guid Identifier { get; private set; }
+        public long AccountId { get; private set; }
+        //public virtual Account Account { get; private set; }
+        public bool IsExpense { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Date { get; private set; }
+        public float Value { get; private set; }
+        public DateTimeOffset CreatedOn { get; private set; }
+        public DateTimeOffset? LastUpdatedOn { get; private set; }
+        public DefaultState Status { get; private set; }
+
+        protected Entry(bool isExpense)
+        {
+            this.Identifier = Guid.NewGuid();
+            this.CreatedOn = DateTimeOffset.Now;
+            this.Status = DefaultState.Active;
+        }
+
+        public Entry(bool isExpense, long accountId, string description, DateTime date, float value)
+            : this(isExpense)
+        {
+            this.AccountId = accountId;
+            this.Description = description;
+            this.Date = date;
+            this.Value = value;
+        }
+
+        public virtual bool Register()
+        {
+            return this.RegisterIsValid();
+        }
+
+        public virtual bool ChangeInfo(string description, DateTime date, float value)
+        {
+            if (this.ChangeInfoIsValid(description, date, value))
+            {
+                this.Description = description;
+                this.Date = date;
+                this.Value = value;
+                this.LastUpdatedOn = DateTimeOffset.Now;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual bool Activate()
+        {
+            if (this.ActivateIsValid())
+            {
+                this.Status = DefaultState.Active;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual bool Deactivate()
+        {
+            if (this.DeactivateIsValid())
+            {
+                this.Status = DefaultState.Inactive;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/MoneyManager.Domain/Scopes/EntryScopes.cs
@@ -0,0 +1,29 @@
+﻿using MoneyManager.Domain.Entities;
+using MoneyManager.Domain.Enums;
+using System;
+
+namespace MoneyManager.Domain.Scopes
+{
+    public static class EntryScopes
+    {
+        public static bool RegisterIsValid(this Entry entity)
+        {
+            return true;
+        }
+
+        public static bool ChangeInfoIsValid(this Entry entity, string description, DateTime date, float value)
+        {
+            return true;
+        }
+
+        public static bool ActivateIsValid(this Entry entity)
+        {
+            return entity.Status != DefaultState.Active;
+        }
+
+        public static bool DeactivateIsValid(this Entry entity)
+        {
+            return entity.Status != DefaultState.Inactive;
+        }
+    }
+}
