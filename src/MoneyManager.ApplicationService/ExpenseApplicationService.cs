ffd2824d856fa85fb66b412f023801a0121da52f
Lucas Rosinelli da Rocha <lucas.rosinelli@gmail.com>
2019-05-06 16:15:11 +0100

    Income and Expense application services added.

---
+++ src/MoneyManager.ApplicationService/ExpenseApplicationService.cs
@@ -0,0 +1,70 @@
+﻿using MoneyManager.Domain.Commands.EntryCommands;
+using MoneyManager.Domain.Contracts.ApplicationServices;
+using MoneyManager.Domain.Contracts.Repositories;
+using MoneyManager.Domain.DataTransferObjects.ExpenseDataTransferObjects;
+using MoneyManager.Domain.Entities;
+using MoneyManager.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MoneyManager.ApplicationService
+{
+    public class ExpenseApplicationService : ApplicationServiceBase<IExpenseRepository, Expense>, IExpenseApplicationService
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public ExpenseApplicationService(IUnitOfWork unitOfWork, IExpenseRepository repository, IAccountRepository accountRepository)
+            : base(unitOfWork, repository)
+        {
+            this._accountRepository = accountRepository;
+        }
+
+        public async Task<ExpenseDetailDataTransferObject> RegisterAsync(RegisterCommand command)
+        {
+            var account = await this._accountRepository.GetByIdentifierAsync(command.Account);
+
+            if (account != null)
+            {
+                var expense = new Expense(account.Id, command.Description, command.Date, command.Value);
+
+                if (expense.Register())
+                {
+                    using (var transaction = this.UnitOfWork.CreateTransaction())
+                    {
+                        expense = await this.Repository.CreateAsync(expense, transaction) as Expense;
+                        if (this.UnitOfWork.Commit(transaction))
+                        {
+                            return new ExpenseDetailDataTransferObject(expense);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public async Task<ExpenseDetailDataTransferObject> GetByIdentifierAsync(Guid account, Guid identifier)
+        {
+            if (await this.Repository.GetByIdentifierAsync(identifier) is Expense expense)
+            {
+                return new ExpenseDetailDataTransferObject(expense);
+            }
+
+            return null;
+        }
+
+        public async Task<IEnumerable<ExpenseDetailDataTransferObject>> GetAsync(Guid account)
+        {
+            var expenses = await this.Repository.GetAsync();
+
+            var resultExpenses = new HashSet<ExpenseDetailDataTransferObject>();
+            foreach (var expense in expenses)
+            {
+                resultExpenses.Add(new ExpenseDetailDataTransferObject(expense as Expense));
+            }
+
+            return resultExpenses;
+        }
+    }
+}
---
+++ src/MoneyManager.Domain/DataTransferObjects/IncomeDataTransferObjects/IncomeDetailDataTransferObject.cs
@@ -0,0 +1,64 @@
+﻿using MoneyManager.Domain.Contracts.DataTransferObjects;
+using MoneyManager.Domain.Entities;
+using MoneyManager.Domain.Enums;
+using System;
+
+namespace MoneyManager.Domain.DataTransferObjects.IncomeDataTransferObjects
+{
+    public class IncomeDetailDataTransferObject : IDataTransferObject
+    {
+        /// <summary>
+        /// Income unique identifier.
+        /// </summary>
+        public Guid Identifier { get; set; }
+        /// <summary>
+        /// Income description.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Income date occurred.
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Income value.
+        /// </summary>
+        public float Value { get; set; }
+        /// <summary>
+        /// Date, time and timezone when the income was created.
+        /// </summary>
+        public DateTimeOffset CreatedOn { get; set; }
+        /// <summary>
+        /// Date, time and timezone when the income data was last updated.
+        /// </summary>
+        public DateTimeOffset? LastUpdatedOn { get; set; }
+        /// <summary>
+        /// Income status.
+        /// </summary>
+        public DefaultState Status { get; set; }
+
+        /// <summary>
+        /// Initializes an empty income.
+        /// </summary>
+        public IncomeDetailDataTransferObject()
+        {
+        }
+
+        /// <summary>
+        /// Initializes an income based on expense entity.
+        /// </summary>
+        /// <param name="income">Income data to be used.</param>
+        public IncomeDetailDataTransferObject(Income income)
+        {
+            if (income != null)
+            {
+                this.Identifier = income.Identifier;
+                this.Description = income.Description;
+                this.Date = income.Date;
+                this.Value = income.Value;
+                this.CreatedOn = income.CreatedOn;
+                this.LastUpdatedOn = income.LastUpdatedOn;
+                this.Status = income.Status;
+            }
+        }
+    }
+}
